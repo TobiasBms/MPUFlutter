77684b76867cde5472d94d18aee33be21bb8382b
TobiasBms <tobiasbms@protonmail.com>
2022-08-08 16:43:40 +0200

    Feature: Added provider package.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'Pages/start_screen.dart';
-void main() {
-  runApp(const MyApp());
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'State/counter.dart';
+
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+      MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create: (_) => Counter()),
+      ],
+      child: const MyApp())
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,8 +28,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "TMA CrossFit",
       theme: ThemeData(
+        textTheme: const TextTheme(
+          button: TextStyle(
+            color: Color(0xbb13090B),
+          )
+        ),
         primarySwatch: Colors.blueGrey,
         colorScheme: const ColorScheme.dark()
       ),
